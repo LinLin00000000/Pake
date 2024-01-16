6cd5b30567183e3a6c2a8d52f49ba752440cd8c6
Lin <i@linlin.me>
2024-01-16 21:10:27 +0800

    ✨ feat: 定制开发了塔科夫地图的功能

--- src-tauri/src/inject/custom.js
+++ src-tauri/src/inject/custom.js
@@ -4,3 +4,169 @@
  * Additionally, you can directly include any script files in this file
  * that you wish to attach to the application.
  */
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (!window._injected) {
+    window._injected = true;
+    // const url = 'https://github.com/LinLin00000000/L-UserScript/raw/main/publish/Tarkov-Realtime-Map.user.js';
+
+    // __TAURI__.http.fetch(url, { responseType: 2 }).then(res => {
+    //   const script = document.createElement('script');
+    //   script.textContent = res.data;
+    //   const head = document.head;
+    //   head.insertBefore(script, head.firstChild);
+    // });
+    let input;
+
+    dynamicQuery('div.page-content', parent => {
+      setInterval(() => {
+        const e = parent.querySelector('div.panel_top.d-flex > div > input[type=text]');
+        if (e === null) {
+          dynamicQuery('div.panel_top.d-flex > div > button', e => setTimeout(() => e.click(), 500), {
+            parent,
+          });
+          dynamicQuery(
+            'div.panel_top.d-flex > div > input[type=text]',
+            e => {
+              input = e;
+            },
+            {
+              parent,
+            },
+          );
+        }
+      }, 2000);
+    });
+
+    __TAURI__.event.listen('gps', event => {
+      const data = event.payload;
+      console.log('Received message:', data);
+      if (input) {
+        input.value = data;
+        input.dispatchEvent(new Event('input', { bubbles: true, cancelable: true }));
+      }
+    });
+  }
+});
+
+// dynamicQuery.ts
+var dynamicQuery = /* @__PURE__ */ (() => {
+  function addObserver(target, callback) {
+    let canceled = false;
+    const observer = new MutationObserver(mutations => {
+      for (const mutation of mutations) {
+        if (mutation.type === 'childList' || mutation.type === 'attributes') {
+          if (canceled) return;
+          callback(mutation.target);
+          for (const node of mutation.addedNodes) {
+            if (canceled) return;
+            callback(node);
+          }
+        }
+      }
+    });
+    observer.observe(target, {
+      subtree: true,
+      childList: true,
+      attributes: true,
+    });
+    return () => {
+      canceled = true;
+      observer.disconnect();
+    };
+  }
+  const observedNodeMap = /* @__PURE__ */ new WeakMap();
+  function addProcessor(target, processor) {
+    let observedNode = observedNodeMap.get(target);
+    if (!observedNode) {
+      let checked = /* @__PURE__ */ new WeakSet();
+      let processors = /* @__PURE__ */ new Set();
+      const checkAndApply = e => {
+        if (checked && !checked.has(e)) {
+          checked.add(e);
+          processors?.forEach(([s, f]) => {
+            if (e.matches(s)) {
+              f(e);
+            }
+          });
+        }
+      };
+      const disconnect = addObserver(target, e => {
+        if (e instanceof Element) {
+          checkAndApply(e);
+          e.querySelectorAll('*').forEach(checkAndApply);
+        }
+      });
+      observedNode = {
+        processors,
+        remove: () => {
+          disconnect();
+          checked = null;
+          processors = null;
+        },
+      };
+      observedNodeMap.set(target, observedNode);
+    }
+    observedNode.processors.add(processor);
+  }
+  function removeProcessor(target, processor) {
+    const observedNode = observedNodeMap.get(target);
+    if (!observedNode) return false;
+    const isDeleteInThisTime = observedNode.processors.delete(processor);
+    if (!observedNode.processors.size) {
+      observedNode.remove();
+      observedNodeMap.delete(target);
+    }
+    return isDeleteInThisTime;
+  }
+  return function (selector, callback = console.log, options = {}) {
+    const {
+      parent = document,
+      once = true,
+      timeout = -1,
+      onTimeout = () => console.log('dynamicQuery Timeout!', arguments),
+      all = true,
+      allDelay = 1e3,
+    } = options;
+    const selectors = Array.isArray(selector) ? selector : [selector];
+    const notExistSelectors = selectors.filter(selector2 => {
+      const result = all
+        ? parent.querySelectorAll(selector2)
+        : [parent.querySelector(selector2)].filter(e => e !== null);
+      result.forEach(callback);
+      return result.length === 0;
+    });
+    if (once && notExistSelectors.length === 0) return () => false;
+    const listenSelectors = once ? notExistSelectors : selectors;
+    const processors = listenSelectors.map(selector2 => {
+      const processed = /* @__PURE__ */ new WeakSet();
+      let timer;
+      const process = e => {
+        if (!processed.has(e)) {
+          processed.add(e);
+          callback(e);
+          if (once) {
+            if (all) {
+              clearTimeout(timer);
+              timer = setTimeout(remove, allDelay);
+            } else {
+              remove();
+            }
+          }
+        }
+      };
+      const processor = [selector2, process];
+      const remove = () => removeProcessor(parent, processor);
+      addProcessor(parent, processor);
+      return remove;
+    });
+    const removeAllProcessor = () => processors.every(f => f());
+    if (timeout >= 0) {
+      setTimeout(() => {
+        removeAllProcessor();
+        onTimeout();
+      }, timeout);
+    }
+    return removeAllProcessor;
+  };
+})();
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,11 +9,17 @@ mod util;
 use app::{invoke, menu, window};
 use invoke::{download_file, download_file_by_binary};
 use menu::{get_menu, menu_event_handle};
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use std::{env, path::PathBuf, sync::mpsc::channel};
+use tauri::Manager;
 use tauri_plugin_window_state::Builder as windowStatePlugin;
+use tokio::fs;
+use tokio::time::{sleep, Duration};
 use util::{get_data_dir, get_pake_config};
 use window::get_window;
 
-pub fn run_app() {
+#[tokio::main]
+async fn main() {
     let (pake_config, tauri_config) = get_pake_config();
     let show_menu = pake_config.show_menu();
     let menu = get_menu();
@@ -39,6 +45,32 @@ pub fn run_app() {
         }
     }
 
+    // 设置要监听的文件夹
+    let home_dir = env::var("HOME")
+        .or_else(|_| env::var("USERPROFILE"))
+        .expect("无法找到 home 目录");
+    let watch_folder = PathBuf::from(home_dir)
+        .join("Documents")
+        .join("Escape from Tarkov")
+        .join("Screenshots");
+
+    // 创建一个通道来接收事件
+    let (tx, rx) = channel();
+
+    // 创建并启动监听器
+    let mut watcher = RecommendedWatcher::new(
+        move |res| {
+            if let Err(e) = tx.send(res) {
+                println!("发送事件时出错: {:?}", e);
+            }
+        },
+        notify::Config::default(),
+    )
+    .unwrap();
+    watcher
+        .watch(&watch_folder, RecursiveMode::Recursive)
+        .unwrap();
+
     tauri_app
         .plugin(windowStatePlugin::default().build())
         .invoke_handler(tauri::generate_handler![
@@ -46,6 +78,47 @@ pub fn run_app() {
             download_file_by_binary
         ])
         .setup(|app| {
+            let app_handle = app.handle();
+            // 在单独的线程中处理文件系统事件
+            tokio::spawn(async move {
+                for res in rx {
+                    match res {
+                        Ok(notify::Event {
+                            kind: notify::EventKind::Create(_),
+                            paths,
+                            ..
+                        }) => {
+                            if let Some(path) = paths
+                                .get(0)
+                                .and_then(|p| p.file_stem())
+                                .and_then(|f| f.to_str())
+                            {
+                                let filename = path.to_string();
+                                println!("检测到新文件: {}", filename); // 输出新文件检测信息
+                                app_handle.emit_all("gps", &filename).unwrap(); // 更改事件名为 "gps"
+
+                                let path_clone = paths[0].clone();
+                                println!("准备删除文件: {:?}", path_clone);
+                                tokio::spawn(async move {
+                                    sleep(Duration::from_secs(3)).await;
+                                    match fs::remove_file(&path_clone).await {
+                                        Ok(_) => println!("文件 {:?} 已删除", path_clone),
+                                        Err(e) => {
+                                            println!("删除文件 {:?} 时出错: {}", path_clone, e)
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                        Ok(_) => {
+                            // 可以选择不执行任何操作或输出一些日志信息
+                            println!("其他类型的文件事件被忽略");
+                        }
+                        Err(e) => println!("监听错误: {:?}", e),
+                    }
+                }
+            });
+
             let _window = get_window(app, pake_config, data_dir);
             // Prevent initial shaking
             _window.show().unwrap();
@@ -66,8 +139,4 @@ pub fn run_app() {
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-fn main() {
-    run_app()
 }
